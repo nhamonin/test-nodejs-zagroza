86500bdf506a2cc8d87e77e1910dca16e4d9d6a3
nhamonin <nazarii.hamonin@gmail.com>
2023-07-14 15:29:33 +0300

    Refactor processPasswordRecovery function to remove unnecessary error handling

--- utils/processPasswordRecovery.js
+++ utils/processPasswordRecovery.js
@@ -4,9 +4,5 @@ import path from 'node:path';
 export function processPasswordRecovery(filePath, type, res) {
   const newFilePath = path.join('uploads', `${type}-${Date.now()}.csv`);
 
-  rename(filePath, newFilePath, (error) => {
-    if (error) {
-      return res.status(400).send({ error: 'Failed to save file' });
-    }
-  });
+  rename(filePath, newFilePath, () => {});
 }
